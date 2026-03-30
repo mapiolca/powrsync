1f91af85956f12384bd121eb0d621c10a15cf857
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-03-30 21:53:17 +0200

    Update setup.php

--- admin/setup.php
+++ admin/setup.php
@@ -65,12 +65,18 @@ if ($action == 'update') {
 // =========================================================================
 // VUE
 // =========================================================================
+$title = $langs->trans('ModuleSetup', 'PowrSync');
+$helpurl = '';
 
-$wikihelp = '';
-llxHeader('', $langs->trans('PowrSyncSetup'), $wikihelp);
+llxHeader('', $title, $helpurl, '', 0, 0, '', '', '', 'mod-timesheetweek page-admin');
 
-$linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans('BackToModuleList').'</a>';
-print load_fiche_titre($langs->trans('PowrSyncSetup'), $linkback, 'title_setup');
+// Subheader
+$linkback = '<a href="'.($backtopage ? $backtopage : DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1').'">'.$langs->trans("BackToModuleList").'</a>';
+print load_fiche_titre($langs->trans($title), $linkback, 'title_setup');
+
+// Configuration header
+$head = powrsyncAdminPrepareHead();
+print dol_get_fiche_head($head, 'settings', $langs->trans($title), -1, 'powrsync@powrsync');
 
 // Récupérer la liste des fournisseurs pour le select
 $sql = "SELECT s.rowid, s.nom FROM ".MAIN_DB_PREFIX."societe s";
