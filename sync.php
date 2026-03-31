f6e1809f6faec8bcb9fe3e69579a9960e6745ac4
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-03-31 21:41:49 +0200

    Update sync.php

--- sync.php
+++ sync.php
@@ -88,7 +88,7 @@ if ($user->hasRight('powrsync', 'synclog', 'write')) {
 // VUE
 // =========================================================================
 
-llxHeader($langs->trans('PowrSyncTitle'), '');
+llxHeader('', $langs->trans('PowrSyncTitle'), '');
 
 //print load_fiche_titre($langs->trans('PowrSyncTitle'), '', 'price');
 print_barre_liste($langs->trans('PowrSync'), $page, $_SERVER['PHP_SELF'], $param, $sortfield, $sortorder, '', $total, $limit, 'fa fa-sun', 0, '', '', $limit);
